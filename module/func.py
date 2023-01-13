5810baa96020a05ebb11579fdc6064ee4e1c576e
pyfbsdk59 <pyfbsdk59@gmail.com>
2023-01-13 11:00:02 +0800

    1st commit

---
+++ module/func.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage, ImageSendMessage, StickerSendMessage, LocationSendMessage, QuickReply, QuickReplyButton, MessageAction
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+
+def sendImageA(event):  #傳送圖片
+    import  json, ssl, urllib.request
+
+    url = 'https://pttbeauty168.zapto.org/api/images/random/'
+    context = ssl._create_unverified_context()
+
+    with urllib.request.urlopen(url, context=context) as jsondata:
+    #將JSON進行UTF-8的BOM解碼，並把解碼後的資料載入JSON陣列中
+        data = json.loads(jsondata.read().decode('utf-8-sig')) 
+
+
+    print(data['id'])
+    print(data['Url'])
+
+
+    try:
+        
+        message = ImageSendMessage(
+            original_content_url = data['Url'],
+            preview_image_url = data['Url']
+        )
+        line_bot_api.reply_message(event.reply_token,message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
---
+++ myapp/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from linebot import LineBotApi, WebhookParser, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage
+from linebot.models import TextSendMessage, ImageSendMessage
+import os
+from module import func
+
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+parser = WebhookParser(os.getenv("LINE_CHANNEL_SECRET"))
+
+
+
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                if isinstance(event.message, TextMessage):
+                    mtext = event.message.text   #傳送文字
+
+                    
+
+                    
+                    if mtext == "ptbr":  
+                        func.sendImageA(event)
+                        
+
+            ##########################
+
+                     
+                                              
+                
+        return HttpResponse()
+
+    else:
+        return HttpResponseBadRequest()
+
+
